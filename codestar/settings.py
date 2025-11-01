c8ae8ff7858839605ed7c3da9fded77d96fab313
John Kingham <196238029+John-Kingham@users.noreply.github.com>
2025-11-01 14:19:49 +0000

    Create Post model

--- codestar/settings.py
+++ codestar/settings.py
@@ -17,6 +17,10 @@ import dj_database_url
 if os.path.isfile("env.py"):
     import env
 
+# This reference to env removes an IDE "unused import" error
+# env is imported so the interpreter runs it and creates environment variables
+env
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (os.environ.get("SECRET_KEY"))
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -89,6 +93,11 @@ WSGI_APPLICATION = "codestar.wsgi.application"
 #     }
 # }
 DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com",
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
