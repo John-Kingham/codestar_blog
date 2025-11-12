b51547ab8598460cc675261bd82109f7f55dd1a3
John Kingham <196238029+John-Kingham@users.noreply.github.com>
2025-11-12 15:27:07 +0000

    feat: add comments to post details page

--- blog/views.py
+++ blog/views.py
@@ -25,5 +25,11 @@ def post_detail(request, slug):
     """
     queryset = Post.objects.filter(status=PUBLISHED)
     post = get_object_or_404(queryset, slug=slug)
-    context = {"post": post, "author": post.author}
+    comments = post.comments.all().order_by("-created_on")
+    comment_count = post.comments.filter(approved=True).count()
+    context = {
+        "post": post,
+        "comments": comments,
+        "comment_count": comment_count,
+    }
     return render(request, "blog/post_detail.html", context)
