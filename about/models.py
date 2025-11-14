349f70f4812b3c4cc2571b7a702350362817c64e
John Kingham <196238029+John-Kingham@users.noreply.github.com>
2025-11-14 14:28:57 +0000

    feat: Add CollaborateRequest model

--- about/models.py
+++ about/models.py
@@ -13,3 +13,14 @@ class About(models.Model):
 
     class Meta:
         ordering = ["-updated_on"]
+
+
+class CollaborateRequest(models.Model):
+
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    message = models.TextField()
+    read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Collaboration request from {self.name}"
