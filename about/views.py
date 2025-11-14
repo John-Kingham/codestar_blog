41c790bf7c4c907addbf1cb10a69163b83e7d2b3
John Kingham <196238029+John-Kingham@users.noreply.github.com>
2025-11-14 16:10:55 +0000

    feat: save collaboration requests

--- about/views.py
+++ about/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.shortcuts import render
 from .forms import CollaborateForm
 from .models import About
@@ -6,9 +7,31 @@ from .models import About
 # Create your views here.
 def about_view(request):
     about = About.objects.first()
+    if request.method == "POST":
+        save_collaboration_request(request)
     collaborate_form = CollaborateForm()
     context = {
         "about": about,
         "collaborate_form": collaborate_form,
     }
     return render(request, "about/about.html", context)
+
+
+def save_collaboration_request(request):
+    """
+    Save a collaboration request to the database.
+    If the request is successful, send a confirmation message to `messages`.
+
+    Args:
+        request (HTTPRequest): The HTTP request
+    """
+    collaboration_form = CollaborateForm(data=request.POST)
+    if collaboration_form.is_valid():
+        collaboration_request = collaboration_form.save(commit=False)
+        collaboration_request.read = False
+        collaboration_request.save()
+        message = (
+            "Collaboration request received! I endeavour to respond "
+            "within 2 working days."
+        )
+        messages.add_message(request, messages.SUCCESS, message)
